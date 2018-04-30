0d2c491e6f22a95073e6b57034d1f1612e53581a
auspbro <auspbro@gmail.com>
2018-04-30 13:54:13 +0800

    Change LED test item name

--- 0WM_BLT1/Test_LED1to6.bat
+++ 0WM_BLT1/Test_LED1to6.bat
@@ -11,7 +11,7 @@
 
 @rem Testing Procedure:
 @rem ==================
-@rem MTP tool sends a command in Linux shell prompt to turn on LED1~6 and then testing fixture reads LEDs status to check whether LEDs are working or not.
+@rem MTP tool sends a command in Linux shell prompt to turn on LED1to61~6 and then testing fixture reads LED1to6s status to check whether LED1to6s are working or not.
 @rem ==================
 
 @rem Linux Command(tool):
@@ -27,27 +27,27 @@
 :START
 CALL .\Process\DVSN.BAT
 
-:LED_Chk
+:LED1to6_Chk
 timeout 3
-msg.exe "检查LED是否都有亮！" 3 700 200 12
+msg.exe "检查LED1to6是否都有亮！" 3 700 200 12
 echo **************************************
-echo ****  Y(1).LED Pass    ****
-echo ****  N(0).LED Fail    ****
-echo ****  R(8).Retest LED  ****
+echo ****  Y(1).LED1to6 Pass    ****
+echo ****  N(0).LED1to6 Fail    ****
+echo ****  R(8).Retest LED1to6  ****
 echo **************************************
 choice /c:Y1N0R8 /N
-if errorlevel 6 goto LED_Chk
-if errorlevel 5 goto LED_Chk
+if errorlevel 6 goto LED1to6_Chk
+if errorlevel 5 goto LED1to6_Chk
 if errorlevel 4 goto fail
 if errorlevel 3 goto fail
 goto Pass
 
 :PASS
 color 2f
->.\log\Test_LED_CheckLog.bat echo set LED=%BT_MAC_ADDRESS%
->>.\log\Test_LED_CheckLog.bat echo set TestResult=PASS
+>.\log\Test_LED1to6_CheckLog.bat echo set LED1to6=%BT_MAC_ADDRESS%
+>>.\log\Test_LED1to6_CheckLog.bat echo set TestResult=PASS
 cd .\Process
-call sdtCheckLog.exe Model_MLBTEST.cfg LED
+call sdtCheckLog.exe Model_MLBTEST.cfg LED1to6
 cd..
 GOTO END
 
@@ -55,10 +55,10 @@ GOTO END
 color 4f
 ECHO ************************************************************
 ECHO *..........................................................*
-ECHO *.................... Check LED FAIL! .....................*
+ECHO *.................... Check LED1to6 FAIL! .....................*
 ECHO *..........................................................*
 ECHO ************************************************************
-MSG "Check LED FAIL!" 6 650 200 15
+MSG "Check LED1to6 FAIL!" 6 650 200 15
 pause
 color 07
 goto end
